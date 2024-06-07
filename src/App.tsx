e67abcc5a97e2710ca590cac9ff448ac2911841b
Nesrux <joaomarcosdevs@gmail.com>
2024-06-07 19:50:53 -0300

    feat: implementa routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
-import { Box, ThemeProvider } from '@mui/material';
+import { Box, ThemeProvider, Typography } from '@mui/material';
 import './App.css';
 import { Header } from './components/Header';
 import { Layout } from './components/Layout';
 import { appTheme } from './config/theme';
+import { Routes, Route, Link } from 'react-router-dom';
+
+const Home = () => (
+  <Box>
+    <Typography variant='h3' component="h3">
+      HOME
+    </Typography>
+  </Box>
+)
+const About = () => (
+  <Box>
+    <Typography variant='h3' component="h3">
+      About
+    </Typography>
+  </Box>
+)
 
 function App() {
   return (
     <ThemeProvider theme={appTheme}>
       <Box component="main" sx={{
         height: "100vh",
+        backgroundColor: (theme) => theme.palette.grey[900],
       }}>
         <Header />
         <Layout >
-          <h1>SALVE SALVE FAMILIAA</h1>
+          <h1>Minha Rota</h1>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/' element={<About />} />
+          </Routes>
         </Layout>
       </Box>
     </ThemeProvider>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Box>
       <Container
-        maxWidth="lg" sx={{ mt: 4, mb: 4, bgcolor:"#000" }}>
+        maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
         {children}
       </Container>
     </Box>
