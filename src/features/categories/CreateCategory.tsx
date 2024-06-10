7adec5585a3191b0a902a0db431fb6f272b18781
Nesrux <joaomarcosdevs@gmail.com>
2024-06-10 11:03:00 -0300

    feat: cria componente genérico de category

--- src/features/categories/CreateCategory.tsx
+++ src/features/categories/CreateCategory.tsx
@@ -1,6 +1,5 @@
-import { Box, Button, FormControl, FormControlLabel, FormGroup, Grid, Paper, Switch, TextField, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import { Category } from "./CategorySlice";
 
 export const CategoryCreate = () => {
@@ -27,72 +26,7 @@ export const CategoryCreate = () => {
             <Typography variant="h4">Create Category</Typography>
           </Box>
         </Box>
-        <Box p={2}>
-          <form>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <TextField
-                    required
-                    name="name"
-                    label="Name"
-                    value={category.name}
-                    disabled={isDisabled}
-                    onChange={handlerChange}>
-
-                  </TextField>
-                </FormControl>
-              </Grid>
-
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <TextField
-                    required
-                    name="description"
-                    label="Description"
-                    value={category.description}
-                    disabled={isDisabled}
-                    onChange={handlerChange}>
-
-                  </TextField>
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <FormGroup>
-                  <FormControlLabel
-                    control={
-                      <Switch
-                        name="is_active"
-                        color="secondary"
-                        onChange={handleToggle}
-                        checked={category.is_active}
-                        inputProps={{ "aria-label": "contolled" }}
-                      />
-                    }
-                    label="Active"
-                  />
-                </FormGroup>
-              </Grid>
-              <Grid item xs={12}>
-                <Box display="flex" gap={2} >
-                  <Button
-                    variant="contained"
-                    component={Link}
-                    to="/categories"
-                    disabled={isDisabled}>Back</Button>
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    color="secondary"
-                    component={Link}
-                    to="/categories"
-                    disabled={isDisabled}>Send</Button>
-
-                </Box>
-              </Grid>
-            </Grid>
-          </form>
-        </Box>
+       
       </Paper>
     </Box>
   );
---
+++ src/features/categories/components/CategoryForm.tsx
@@ -0,0 +1,91 @@
+import { Box, Button, FormControl, FormControlLabel, FormGroup, Grid, Switch, TextField } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Category } from '../CategorySlice';
+
+
+type Props = {
+  category: Category;
+  isDisable?: boolean;
+  isLoading?: boolean;
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  handleToggle: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+export function CategoryForm({
+  category,
+  isDisable = false,
+  isLoading = false,
+  onSubmit,
+  handleChange,
+  handleToggle
+}: Props) {
+
+  return (
+    <Box p={2}>
+      <form>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <TextField
+                required
+                name="name"
+                label="Name"
+                value={category.name}
+                disabled={isDisable}
+                onChange={handleChange}>
+
+              </TextField>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <TextField
+                required
+                name="description"
+                label="Description"
+                value={category.description}
+                disabled={isDisable}
+                onChange={handleChange}>
+
+              </TextField>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Switch
+                    name="is_active"
+                    color="secondary"
+                    onChange={handleToggle}
+                    checked={category.is_active}
+                    inputProps={{ "aria-label": "contolled" }}
+                  />
+                }
+                label="Active"
+              />
+            </FormGroup>
+          </Grid>
+          <Grid item xs={12}>
+            <Box display="flex" gap={2} >
+              <Button
+                variant="contained"
+                component={Link}
+                to="/categories"
+                disabled={isDisable}>Back</Button>
+              <Button
+                type="submit"
+                variant="contained"
+                color="secondary"
+                component={Link}
+                to="/categories"
+                disabled={isDisable}>Send</Button>
+
+            </Box>
+          </Grid>
+        </Grid>
+      </form>
+    </Box>
+  )
+}
